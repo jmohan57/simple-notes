5f5857afc55967b2584a17db9876b7615b3385d5
jmohan57 <jeffreymohan57@gmail.com>
2023-08-12 23:39:48 +0530

    debug

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -4,12 +4,9 @@ import { NextRequest } from "next/server";
 
 export function getDataFromToken(request: NextRequest) {
   try {
-    const token = request.cookies.get("token");
-    let data: ITokenData;
-    if (token) {
-      data = jwt.verify(token.value, process.env.TOKEN_SECRET!) as ITokenData;
-      return data;
-    }
+    const token = request.cookies.get("token")?.value || "";
+    const data: ITokenData = jwt.verify(token, process.env.TOKEN_SECRET!) as ITokenData;
+    return data;    
   } catch (error: any) {
     throw new Error(error);
   }
