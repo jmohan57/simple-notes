312114db46f2d88eceedf2065acef1fd5f8fcf78
jmohan57 <jeffreymohan57@gmail.com>
2023-08-13 00:04:28 +0530

    debug

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -52,7 +52,7 @@ export async function POST(request: NextRequest) {
       success: true,
     });
 
-    response.cookies.set("token", token, { httpOnly: true });
+    response.cookies.set("token", token);
 
     return response;
   } catch (error) {
