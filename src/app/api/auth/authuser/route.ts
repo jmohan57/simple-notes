3a0087444affc03467b242755f55f804f366591f
jmohan57 <jeffreymohan57@gmail.com>
2023-08-13 00:15:10 +0530

    debug

--- src/app/api/auth/authuser/route.ts
+++ src/app/api/auth/authuser/route.ts
@@ -1,25 +1,43 @@
 import { connect } from "@/dbconfig/dbConfig";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 import User from "@/models/userModel";
+import { ITokenData } from "@/types/token-data-interface";
+import jwt from "jsonwebtoken";
 import { NextRequest, NextResponse } from "next/server";
 
 connect();
 
 export async function GET(request: NextRequest) {
   try {
-    const userId = getDataFromToken(request)?.id;
-    const user = await User.findById(userId).select("-password");
+    const token = request.cookies.get("token")?.value || "";
+    if (token.length > 0) {
+      const data: ITokenData = jwt.verify(
+        token,
+        process.env.TOKEN_SECRET!
+      ) as ITokenData;
+      const userId = data.id;
+      const user = await User.findById(userId).select("-password");
 
-    if (user) {
-      return NextResponse.json({
-        message: "User Details Found",
-        success: true,
-        data: user,
-      });
+      if (user) {
+        return NextResponse.json({
+          message: "User Details Found",
+          success: true,
+          data: user,
+        });
+      } else {
+        // Remove token from cookies
+        const response = NextResponse.json({
+          message: "Invalid Token !",
+          success: false,
+          staus: 400,
+        });
+        response.cookies.delete("token");
+        return response;
+      }
     } else {
       // Remove token from cookies
       const response = NextResponse.json({
-        message: "Invalid Token !",
+        message: "Can not access token !",
         success: false,
         staus: 400,
       });
